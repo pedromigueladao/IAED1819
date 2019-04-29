369bf0dadccb3c43393a82095ed3a38e8760b514
mikolas <mikolas.janota@gmail.com>
2019-04-29 21:50:59 +0100

    lab10

---
+++ labs/lab10/test_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# File:  test_all.sh
+# Author:  mikolas
+# Created on:  Sat Feb 23 13:04:25 STD 2019
+# Copyright (C) 2019, Mikolas Janota
+#
+failures=0
+for e in ex01 ex02; do
+    ./test.sh ${e}
+    let failures="$?+${failures}"
+done
+echo FAILURES: $failures
+if [ ${failures} == 0 ]; then
+    echo "**********************************"
+    echo "*All tests passed! Great success!*"
+    echo "**********************************"
+fi
